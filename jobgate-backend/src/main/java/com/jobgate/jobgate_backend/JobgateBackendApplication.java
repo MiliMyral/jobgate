54831f51d4074892309e26a0426c6e92dd7cf534
MiliMyral <milimyral@gmail.com>
2026-06-29 21:34:54 +0100

    feat: initialisation projet Spring Boot - Jobgate Backend

---
+++ jobgate-backend/src/main/java/com/jobgate/jobgate_backend/JobgateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jobgate.jobgate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobgateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobgateBackendApplication.class, args);
+	}
+
+}
---
+++ jobgate-backend/src/test/java/com/jobgate/jobgate_backend/JobgateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobgate.jobgate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobgateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
